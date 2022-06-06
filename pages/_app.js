077a91fae2e516d192187aa047b8c909ae506575
lpohsien <popopo4407@gmail.com>
2022-06-06 19:21:23 +0800

    Add location access in navbar

--- pages/_app.js
+++ pages/_app.js
@@ -11,13 +11,31 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter()
   const [user, setUser] = useState(null);
 
+  const initialPos = {lat: '0.000000', lon: '0.000000' }
+  const [currPos, setCurrPos] = useState(initialPos) 
+
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       async () => {
         checkUser()
       }
     )
+
     checkUser()
+
+    if(!navigator.geolocation) {
+      alert("Your browser does not support geolocation, which is an essential feature of Event Horizon. To continue, \
+        please use a different browser!")
+      return (<div>
+        <p>Error!</p>
+      </div>)
+    }
+
+    const watchID = navigator.geolocation.watchPosition((position) => {
+      setCurrPos({ ...currPos, lat:position.coords.latitude, lon:position.coords.longitude, });
+      console.log(position)
+    })
+  
     return () => {
       authListener?.unsubscribe()
     };
@@ -34,44 +52,49 @@ function MyApp({ Component, pageProps }) {
     supabase.auth.signOut()
   }
 
+
   return (
     <UserProvider>
       <div>
-        <nav className="p-6 border-b border-gray-300">
-          <Link href="/main-page">
-            <span className="button">Home</span>
-          </Link>
-          {
-            user && (
-              <div style={{display: 'inline'}}>
-                <Link href="/create-post">
-                  <span className="button">Create Post</span>
-                 </Link>
-                <Link href="/profile">
-                  <span className="button">Profile</span>
-                </Link>
-                <Link href="/settings">
-                  <span className="button">Settings</span>
-                </Link>
-                <Link href="/main-page">
-                  <span className="button" onClick={logout}>Log Out</span>
-                </Link>
-              </div>
-            )
-          } 
-          {
-            !user && (
-              <div style={{display: 'inline'}}>
-                <Link href="/login">
-                  <span className="button">Sign In</span>
-                </Link>
-                <Link href="/login">
-                  <span className="button">Sign Up</span>
-                </Link>
-              </div>
-            ) 
-          }
-
+        <nav className="p-6 border-b border-gray-300" style={{alignItems: "center", display: "flex", justifyContent: "space-between"}}>
+          <span>
+            <Link href="/main-page">
+              <span className="button">Home</span>
+            </Link>
+            {
+              user && (
+                <span>
+                  <Link href="/create-post">
+                    <span className="button">Create Post</span>
+                  </Link>
+                  <Link href="/profile">
+                    <span className="button">Profile</span>
+                  </Link>
+                  <Link href="/settings">
+                    <span className="button">Settings</span>
+                  </Link>
+                  <Link href="/main-page">
+                    <span className="button" onClick={logout}>Log Out</span>
+                  </Link>
+                </span>
+              )
+            } 
+            {
+              !user && (
+                <span>
+                  <Link href="/login">
+                    <span className="button">Sign In</span>
+                  </Link>
+                  <Link href="/login">
+                    <span className="button">Sign Up</span>
+                  </Link>
+                </span>
+              ) 
+            }
+          </span>
+          <span style={{display: "inline-block"}}>
+              Lat:{currPos.lat} Lon:{currPos.lon}
+          </span>
         </nav>
         <div className="py-8 px-16">
           <Component {...pageProps} />
