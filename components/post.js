885c61011215111233b76ac762bbdb300f2f902a
lpohsien <popopo4407@gmail.com>
2022-06-08 02:08:31 +0800

    Add feature to truncate content if too long

--- components/post.js
+++ components/post.js
@@ -1,9 +1,19 @@
 import styles from "../styles/Post.module.css"
 
 export default function Post({ idx, data }) {
+    
+    // Convert timestamp to human readable time
     const timestampDate = new Date(data.createdAt)
     const date = timestampDate.toDateString()
     const time = timestampDate.toLocaleTimeString()
+
+    // Truncate content to MAX_CONTENT_LENGTH
+    const MAX_CONTENT_LENGTH = 1200
+    let truncatedContent = String(data.text)
+    truncatedContent = truncatedContent.length > MAX_CONTENT_LENGTH 
+        ? truncatedContent.substring(0, MAX_CONTENT_LENGTH) + "..."
+        : truncatedContent
+
     return (
         <div key={idx} className={styles.container}>
             <div className={styles.media}>
@@ -15,7 +25,7 @@ export default function Post({ idx, data }) {
                     <div className={styles.stamp}>{data.latitude}, {data.longitude}</div>
                     <div className={styles.stamp}>{date} {time}</div>
                 </header>
-                    {data.text}
+                    {truncatedContent}
                 <div className={styles.footer}>
                     <button className={styles.vote} style={{border: "1px solid orange"}}>-{data.downvotes}</button>
                     <p className={styles.score}>Score: {data.score}</p>
