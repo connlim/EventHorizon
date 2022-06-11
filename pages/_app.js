5e122be7b22ec688151808d377e72a28518e9a67
Connor Lim <dev@connorlim.net>
2022-06-11 20:48:22 +0800

    ui: use bootstrap in navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { supabase } from "../utils/supabaseClient";
 import UserProvider from "../context/user";
+import CustomNavbar from "../components/navbar";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
@@ -29,6 +29,7 @@ function MyApp({ Component, pageProps }) {
 
   function logout() {
     supabase.auth.signOut();
+    router.push("/main-page");
   }
 
   return (
@@ -38,68 +39,7 @@ function MyApp({ Component, pageProps }) {
       </Head>
       <UserProvider>
         <div>
-          <nav
-            className="p-6 border-b border-gray-300"
-            style={{
-              alignItems: "center",
-              display: "flex",
-              justifyContent: "space-between",
-            }}
-          >
-            <span>
-              <Link href="/main-page">
-                <span className="button">Home</span>
-              </Link>
-              {user && (
-                <span>
-                  <Link href="/create-post">
-                    <span className="button">Create Post</span>
-                  </Link>
-                  <Link href="/profile">
-                    <span className="button">Profile</span>
-                  </Link>
-                  <Link href="/settings">
-                    <span className="button">Settings</span>
-                  </Link>
-                  <Link href="/main-page">
-                    <span className="button" onClick={logout}>
-                      Log Out
-                    </span>
-                  </Link>
-                </span>
-              )}
-              {!user && (
-                <span>
-                  <Link href="/login">
-                    <span className="button">Sign In</span>
-                  </Link>
-                  <Link href="/login">
-                    <span className="button">Sign Up</span>
-                  </Link>
-                </span>
-              )}
-            </span>
-            <button
-              type="button"
-              className="mb-4 bg-green-600 text-white font-semibold px-8 py-2 rounded-lg"
-              onClick={() => {
-                if (!navigator) {
-                  alert("No navigator service available");
-                } else {
-                  navigator.geolocation.getCurrentPosition((loc) => {
-                    alert(
-                      "Latitude: " +
-                        loc.coords.latitude +
-                        ", Longitude: " +
-                        loc.coords.longitude
-                    );
-                  });
-                }
-              }}
-            >
-              Show My Position
-            </button>
-          </nav>
+          <CustomNavbar user={user} logout={logout}></CustomNavbar>
           <div className="py-8 px-16">
             <Component {...pageProps} />
           </div>
@@ -110,4 +50,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
