fc34bbb08465027004797d3919133b1ee2d50f6b
Connor Lim <dev@connorlim.net>
2022-06-14 16:18:44 +0800

    ui: use bootstrap design in create post page

--- pages/create-post.js
+++ pages/create-post.js
@@ -1,131 +1,115 @@
 // pages/create-post.js
-import { useState } from 'react'
-import { useRouter } from 'next/router'
-import { supabase } from '../utils/supabaseClient'
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { supabase } from "../utils/supabaseClient";
+import { Button, Container, Form, Stack } from "react-bootstrap";
 
-const initialState = { username: '', content: '', lat: 0.000000, lon: 0.000000 }
+const initialState = { username: "", content: "", lat: 0.0, lon: 0.0 };
 
 function CreatePost() {
-  const [post, setPost] = useState(initialState)
-  let { username, content, lat, lon } = post
-  const router = useRouter()
-
+  const [post, setPost] = useState(initialState);
+  let { username, content, lat, lon } = post;
+  const router = useRouter();
 
   function onChange(e) {
-    setPost(() => ({ ...post, [e.target.name]: e.target.value,}))
+    setPost(() => ({ ...post, [e.target.name]: e.target.value }));
   }
 
-  
   async function createNewPost() {
-    if (!content) return
-    
-    const user = supabase.auth.user()
+    if (!content) return;
 
-    const { data: userInfo} = await supabase
-      .from('profiles')
+    const user = supabase.auth.user();
+
+    const { data: userInfo } = await supabase
+      .from("profiles")
       .select(`username`)
-      .single()
+      .single();
 
-    if(!userInfo) {
-      alert("Please set up your account first!")
-      router.push("/settings")
+    if (!userInfo) {
+      alert("Please set up your account first!");
+      router.push("/settings");
       return;
     }
-    username = userInfo.username
+    username = userInfo.username;
 
-    if(!navigator) { 
-      alert("No navigator service available")
-      return
+    if (!navigator) {
+      alert("No navigator service available");
+      return;
     } else {
       navigator.geolocation.getCurrentPosition(async (position) => {
         const { data, error, status } = await supabase
-        .from('posts')
-        .insert([
-            { text: content, 
+          .from("posts")
+          .insert([
+            {
+              text: content,
               username: username,
               user_id: user.id,
               latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            }
-        ])
-        .single()
+              longitude: position.coords.longitude,
+            },
+          ])
+          .single();
 
-        console.log(post)
-        if(status == 201 && !error) {
-          alert("Posted successfully!")
-          router.push("/main-page")
+        console.log(post);
+        if (status == 201 && !error) {
+          alert("Posted successfully!");
+          router.push("/main-page");
         } else {
-          alert(error)
+          alert(error);
         }
-      })
+      });
     }
-
   }
 
-
   return (
-    <div>
-        <h1 className="text-3xl font-semibold tracking-wide mt-6"
-            style={{
-              textAlign: "center"
-            }}>Create new post</h1>
-        <textarea
-            onChange={onChange}
-            name="content"
-            placeholder="Your content"
-            value={post.content}
-            // className="border-b pb-2 text-lg my-4 focus:outline-none w-full font-light text-gray-500 placeholder-gray-500 y-2"
-            style={{
-              border: "1px solid white",
-              display: "block",
-              marginLeft: "auto",
-              marginRight: "auto",
-              backgroundColor: "black",
-              height: "640px",
-              width: "1300px"
-            }}
-        /> 
-        <button
+    <Container className="mt-3">
+      <Stack gap={3}>
+        <h1 className="mb-3">Create new post</h1>
+        <Form.Control
+          as="textarea"
+          onChange={onChange}
+          name="content"
+          placeholder="Your post"
+          value={post.content}
+        />
+        <Stack direction="horizontal" gap={3}>
+          <Button type="button" onClick={createNewPost}>
+            Create Post
+          </Button>
+          <Button
             type="button"
-            className="mb-4 bg-green-600 text-white font-semibold px-8 py-2 rounded-lg"
-            onClick={createNewPost}
-            style={{
-              border: "1px solid white",
-              display: "block",
-              marginTop: "2rem",
-              marginLeft: "auto",
-              marginRight: "auto",
-              backgroundColor: "black",
-            }}
-        >Create Post</button>
-        <button
-            type="button"
-            className="mb-4 bg-green-600 text-white font-semibold px-8 py-2 rounded-lg"
-            style={{
-              border: "1px solid white",
-              display: "block",
-              marginTop: "1rem",
-              marginLeft: "auto",
-              marginRight: "auto",
-              backgroundColor: "black",
-            }}
+            variant="outline-secondary"
             onClick={() => {
-              navigator.permissions.query({name: 'geolocation'})
+              navigator.permissions
+                .query({ name: "geolocation" })
                 .then((result) => {
-                  if(result.state == 'granted') {
-                    navigator.geolocation.getCurrentPosition((position) => {
-                      setPost(() => ({ ...post, lat: position.coords.latitude, lon: position.coords.longitude}))
-                      console.log(post.lat + ", " + post.lon)
-                    }, (error) => console.log("Error due to " + error))
+                  if (result.state == "granted") {
+                    navigator.geolocation.getCurrentPosition(
+                      (position) => {
+                        setPost(() => ({
+                          ...post,
+                          lat: position.coords.latitude,
+                          lon: position.coords.longitude,
+                        }));
+                        console.log(post.lat + ", " + post.lon);
+                      },
+                      (error) => console.log("Error due to " + error)
+                    );
                   } else {
-                    console.log('Browser location services disabled', navigator);
+                    console.log(
+                      "Browser location services disabled",
+                      navigator
+                    );
                   }
-                })
-              }
-            }
-        >Test Location</button>
-    </div>
-  )
+                });
+            }}
+          >
+            Test Location
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
