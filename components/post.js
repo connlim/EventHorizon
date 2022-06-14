1d140be037d3bafa5923f6c4da680b68acdd852b
Connor Lim <dev@connorlim.net>
2022-06-14 17:17:27 +0800

    ui: update posts to use bootstrap styling

--- components/post.js
+++ components/post.js
@@ -1,38 +1,54 @@
-import styles from "../styles/Post.module.css"
+import { Button, Card, Col, Container, Row, Stack } from "react-bootstrap";
 
 export default function Post({ idx, data }) {
-    
-    // Convert timestamp to human readable time
-    const timestampDate = new Date(data.createdAt)
-    const date = timestampDate.toDateString()
-    const time = timestampDate.toLocaleTimeString()
+  // Convert timestamp to human readable time
+  const timestampDate = new Date(data.createdAt);
+  const date = timestampDate.toDateString();
+  const time = timestampDate.toLocaleTimeString();
 
-    // Truncate content to MAX_CONTENT_LENGTH
-    const MAX_CONTENT_LENGTH = 1200
-    let truncatedContent = String(data.text)
-    truncatedContent = truncatedContent.length > MAX_CONTENT_LENGTH 
-        ? truncatedContent.substring(0, MAX_CONTENT_LENGTH) + "..."
-        : truncatedContent
+  // Truncate content to MAX_CONTENT_LENGTH
+  const MAX_CONTENT_LENGTH = 1200;
+  let truncatedContent = String(data.text);
+  truncatedContent =
+    truncatedContent.length > MAX_CONTENT_LENGTH
+      ? truncatedContent.substring(0, MAX_CONTENT_LENGTH) + "..."
+      : truncatedContent;
 
-    return (
-        <div key={idx} className={styles.container}>
-            <div className={styles.media}>
-                <p>Insert Photo Here!</p>
-            </div>
-            <div className={styles.postDetails}>
-                <header className={styles.header}>
-                    <div className={styles.userName}>{data.username}</div>
-                    <div className={styles.stamp}>{data.latitude}, {data.longitude}</div>
-                    <div className={styles.stamp}>{date} {time}</div>
-                </header>
-                    {truncatedContent}
-                <div className={styles.footer}>
-                    <button className={styles.vote} style={{border: "1px solid orange"}}>-{data.downvotes}</button>
-                    <p className={styles.score}>Score: {data.score}</p>
-                    <button className={styles.vote} style={{border: "1px solid green"}}>+{data.upvotes}</button>
+  return (
+    <Card key={idx} className="w-lg-75">
+      <Card.Img variant="top" src="" />
+      <Card.Body>
+        <Card.Text>{truncatedContent}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <Container fluid>
+          <Row className="align-items-center">
+            <Col className="me-auto">
+              <Stack>
+                <div>{data.username}</div>
+                <div className="text-muted">
+                  {date} {time}
                 </div>
-            </div>
-        </div>
-
-    )
+                <div className="text-muted small">
+                  {data.latitude}, {data.longitude}
+                </div>
+              </Stack>
+            </Col>
+            <Col>
+              <Stack direction="horizontal" gap={3}>
+                <Button className="ms-auto" variant="outline-danger" size="sm">
+                  -{data.downvotes}
+                </Button>
+                <div>{data.score}</div>
+                <Button variant="outline-success" size="sm">
+                  +{data.upvotes}
+                </Button>
+              </Stack>
+            </Col>
+          </Row>
+        </Container>
+      </Card.Footer>
+    </Card>
+  );
 }
+
--- pages/main-page.js
+++ pages/main-page.js
@@ -1,78 +1,78 @@
-import { useState, useEffect } from 'react'
-import { supabase } from '../utils/supabaseClient'
-import { useUser } from '../context/user'
-import Post from '../components/post'
+import { useState, useEffect } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
+import { supabase } from "../utils/supabaseClient";
+import { useUser } from "../context/user";
+import Post from "../components/post";
 
 export default function MainPage() {
+  const { user } = useUser(true);
 
-  const { user } = useUser(true)
+  const [userEmail, setEmail] = useState("null");
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState(null);
 
-  const [userEmail, setEmail] = useState("null")
-  const [loading, setLoading] = useState(false)
-  const [posts, setPosts] = useState(null)
-
-  useEffect(()=>{
-    if(user!=null) setEmail(user.email)
-    else setEmail("null")
-    getAllPost()
+  useEffect(() => {
+    if (user != null) setEmail(user.email);
+    else setEmail("null");
+    getAllPost();
   }, []);
 
-  function getLongAndLat() {
-    
-  }
+  function getLongAndLat() {}
 
   async function getAllPost() {
     try {
-      setLoading(true)
-      const user = supabase.auth.user()
-      if(user!=null) setEmail(user.email)
+      setLoading(true);
+      const user = supabase.auth.user();
+      if (user != null) setEmail(user.email);
 
-      const maxDist = 2 //km
-      const earthRadius = 40075.04 
-      let position = await new Promise((resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject))
-      let { coords } = position
-      const myLat = coords.latitude
-      const myLon = coords.longitude
+      const maxDist = 2; //km
+      const earthRadius = 40075.04;
+      let position = await new Promise((resolve, reject) =>
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      );
+      let { coords } = position;
+      const myLat = coords.latitude;
+      const myLon = coords.longitude;
 
-      const degToRad = x => x / 180 * Math.PI
-      const maxDiffLat = 360 * maxDist / earthRadius //deg
-      const maxDiffLon = 360 * maxDist / (earthRadius * Math.cos(degToRad(myLat)))
-
+      const degToRad = (x) => (x / 180) * Math.PI;
+      const maxDiffLat = (360 * maxDist) / earthRadius; //deg
+      const maxDiffLon =
+        (360 * maxDist) / (earthRadius * Math.cos(degToRad(myLat)));
 
       let { data, error, status } = await supabase
-        .from('posts')
+        .from("posts")
         .select(`*`)
-        .lte('latitude', myLat + maxDiffLat)
-        .gte('latitude', myLat - maxDiffLat)
-        .lte('longitude', myLon + maxDiffLon)
-        .gte('longitude', myLon - maxDiffLon)
-        .order('createdAt', { ascending: false })
+        .lte("latitude", myLat + maxDiffLat)
+        .gte("latitude", myLat - maxDiffLat)
+        .lte("longitude", myLon + maxDiffLon)
+        .gte("longitude", myLon - maxDiffLon)
+        .order("createdAt", { ascending: false });
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
-      setPosts(data)
-
+      setPosts(data);
 
       // if (data) {
       //   setUsername(data.username)
       //   setWebsite(data.website)
       //   setAvatarUrl(data.avatar_url)
       // }
-
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  } 
-
+  }
 
   return (
-    <div className="container" id="root" style={{ padding: '50px 0 100px 0' }}>
-        {posts?.map((entry,idx) => (<Post data={entry} idx={idx}/>))}
-    </div>
-  )
+    <Container id="root" className="d-flex justify-content-center">
+      {posts?.map((entry, idx) => (
+            <Post data={entry} idx={idx} />
+      ))}
+    </Container>
+  );
 }
+
